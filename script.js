88b3d0561c17cc18faaa66233e91299caf2d7b66
Arneelus Lewis <arneelus777@gmail.com>
2020-10-19 16:27:44 -0400

    added animated arrow button to pages

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("working!");
+const downArrowWrapper = document.getElementById("down-arrow-wrapper");
+const downArrow = document.getElementById("down-arrow");
+
+let scrollTop = document.body.scrollTop;
+
+downArrowWrapper.addEventListener("click", function (e) {
+  //   console.log(this);
+  //   console.log(downArrow);
+  //   downArrow.style.color = "blue";
+  //   document.body.transition = "all .3s ease-in-out";
+  //   document.documentElement.scrollTop = 1000;
+});
